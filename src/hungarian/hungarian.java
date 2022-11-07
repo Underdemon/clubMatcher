5508a1fb6727b2324c72d46f42eaf69aabd4de4c
Underdemon <rayanakhan05@gmail.com>
2022-11-07 21:38:09 +0000

    resume hungarian @ if(0 on same row as 0*)

--- src/hungarian/hungarian.java
+++ src/hungarian/hungarian.java
@@ -13,12 +13,14 @@ public class hungarian
 {
     int[][] cost_matrix;
     
+    
     // markers
     
-    // marks row/column element that has been reduced to a 0 with a "square"
-    int[] zero_row, zero_col;
-    //
+    int[] row_star, col_star;
+    
     int[] rowCovered, colCovered;
+    
+    int[] rowPrime, colPrime;
     
     public hungarian(int[][] matrix)
     {
@@ -26,7 +28,7 @@ public class hungarian
     }
     
     /**
-     * Step 1 - row reduction
+     * Step 1 - Row Reduction
      * 
      * ensures there's at least one 0 in a row
      * achieved by subtracting the minima from each number in the row
@@ -50,14 +52,17 @@ public class hungarian
     }
     
     /**
-     * Step 2 - column reduction
+     * Step 2 - Column Reduction
      * 
      * ensures there's at least one 0 in a column
      * achieved by subtracting the minima from each number in the column
+     * 
+     * note: // can use cost_matrix[0] since matrix must be square
      */
     private void col_reduction()
     {
-        for(int i = 0; i < cost_matrix[0].length; ++i)  // can use cost_matrix[0] since matrix must be square
+        // for(int i = 0; i < cost_matrix[0].length; ++i)
+        for(int i : cost_matrix[0])
         {
             int colMin = Integer.MAX_VALUE;
             for(int j = 0; j < cost_matrix.length; ++j)
@@ -74,25 +79,27 @@ public class hungarian
     }
     
     /**
-     * Step 3 - Zero Marking
+     * Step 3 - Zero Marking/Initial Task Assignments
      * 
      * mark all reduced values that are equal to 0 with a "star"
      * one zero per row and column (one zero per worker job pair)
      */
-    private void zeroMarker()
+    private void init_task_assign()
     {
         int[] row_has_starred_zero = new int[cost_matrix.length];
         int[] col_has_starred_zero = new int[cost_matrix[0].length];
         
         for(int i = 0; i < cost_matrix.length; ++i)
         {
-            for(int j = 0; j < cost_matrix[0].length; ++j)
+            // for(int j = 0; j < cost_matrix[0].length; ++j)
+            for(int j : cost_matrix[0])
             {
+                
                 /**
                  * if:
-                 *      current (reduced) value is == 0
-                 *      no already marked 0s in the row
-                 *      no already marked 0s in the column
+                 *      current (reduced) value is == 0         (minimum relative cost)
+                 *      no already marked 0s in the row         (one 0 per row)
+                 *      no already marked 0s in the column      (one 0 per column)
                  */
                 
                 if(cost_matrix[i][j] == 0 && row_has_starred_zero[i] == 0 && col_has_starred_zero[j] == 0)
@@ -104,9 +111,9 @@ public class hungarian
                     col_has_starred_zero[j] = 1;
                     
                     // save row position of 0
-                    zero_row[i] = j;
+                    row_star[i] = j;
                     // save column position of 0
-                    zero_col[j] = i;
+                    col_star[j] = i;
                     
                     continue;   // next row ( a zero has been discovered )
                 }
@@ -117,12 +124,102 @@ public class hungarian
     /**
      * Step 4 - Covering the 0
      * 
-     * cover all columns that have "stars"
+     * cover all columns that have "stars" by marking it with a 1
      */
-    private void zeroCover()
+    private void cover_zero_col()
     {
-        for(int i = 0; i < )
+        // for(int i = 0; i < col_star.length; ++i)
+        for(int i : col_star)
+        {
+            /**
+             * ternary conditional operators
+             * 
+             * https://stackoverflow.com/a/10336927
+             * https://en.wikipedia.org/wiki/Ternary_conditional_operator
+             * 
+             * when to use it (can use it on chained condition - multiple conditions - but don't use it on nested conditions)
+             * https://stackoverflow.com/questions/160218/to-ternary-or-not-to-ternary
+             * 
+             * ==============================
+             * TRANSLATION OF STATEMENT BELOW
+             * ==============================
+             * 
+             * colCovered[i] = col_star[i] != -1 ? 1 : 0;
+             * 
+             * ================================
+             * IS TRANSLATED TO (IN LONG FORM) |
+             * ================================
+             * 
+             * if(col_star[i] != -1         // all arrays filled with -1 as default values
+             * {
+             *      colCovered[i] = 1;
+             * }
+             * else
+             * {
+             *      colCovered[i] = 0;
+             * }
+             * 
+             */
+            
+            colCovered[i] = col_star[i] != -1 ? 1: 0;
+        }
+    }
+    
+    /**
+     * Step 5 - Priming the Uncovered
+     * 
+     * find a non-covered 0 and "prime" it
+     * 
+     * if the to-be-primed 0 is on the same row as an already starred 0 then:
+     *      cover the corresponding row
+     *      uncover the column of the starred 0
+     */
+    private void prime_uncovered()
+    {
+        for(int i = 0; i < cost_matrix.length; ++i)
+        {
+            for(int j : cost_matrix[0])
+            {
+                if(cost_matrix[i][j] == 0 && colCovered[i] == 0)    // if there is a non-covered 0
+                {
+                    rowPrime[j] = 1;
+                    colPrime[i] = 1;
+
+                    if()    // if the 0* is on the same row as the primed 0
+                    {
+                        rowCovered[j] = 1;
+                        //uncover the row of 0*
+                    }
+                }
+            }
+        }
     }
     
     
+    /**
+     * The Ultimate Step - Final Destination
+     * 
+     * If all columns are covered (there is one 0 in each column)
+     * Then the optimal solution to the assignment problem has been solved
+     */
+    private boolean is_all_columns_covered()
+    {
+        for(int i : colCovered)
+        {
+            if(i == 0)
+                return false;
+        }
+        return true;
+    }
 }
+
+/*
+    GENERAL NOTES:
+
+    enhanced for loops used in 1D arrays
+        2D arrays would require nested enhanced for loops, at which point it is easier to use a normal for loop
+
+    LINE 183:
+        colCovered[i] == 0 OR colCovered[j] == 0
+        INDEX I OR J????
+*/
